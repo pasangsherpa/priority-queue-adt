e76dc0d409278a30777dc53acfb8ae724607618a
Pasang Sherpa <pasang_sh@hotmail.com>
2014-08-28 23:23:39 -0700

    boilerplate added

--- src/priority-queue-adt.js
+++ src/priority-queue-adt.js
@@ -1,6 +1,41 @@
 (function() {
     'use strict';
-    var PriorityQueue = {};
+    var PriorityQueue = function() {
+    	var count = 0;
+
+    	function add() {
+
+    	}
+
+    	function remove() {
+
+    	}
+
+    	function contains() {
+
+    	}
+
+    	function getRoot() {
+
+    	}
+
+    	function isEmpty() {
+    		return count === 0;
+    	}
+
+    	function size() {
+    		return count;
+    	}
+
+    	return {
+    		add: add,
+    		remove: remove,
+    		contains: contains,
+    		getRoot: getRoot,
+    		isEmpty: isEmpty,
+    		size: size
+    	}
+    }
 
     if (typeof define === 'function' && define.amd) {
         define(function() {

1b5dca9bdf222e63bb3c401864e10d0c2200d098
Pasang Sherpa <pasang_sh@hotmail.com>
2014-08-28 23:27:48 -0700

    initial test added

--- test/test.js
+++ test/test.js
@@ -1,4 +1,13 @@
 /*global describe, it */
 'use strict';
 var assert = require('assert');
-var qs = require('../src/priority-queue-adt.js');
+var PriorityQueue = require('../src/priority-queue-adt.js');
+
+var pq = new PriorityQueue();
+
+describe('PriorityQueue operations test', function () {
+	it('should create an empty priority-queue in the beginning', function(){
+		assert.equal(pq.isEmpty(), true);
+		assert.equal(pq.size(), 0);
+	});
+});
